3d462d3ea6358afe37fbbab705c0b9f05a7cde7e
bedada <bbekele@excellerentsolutions.com>
2024-02-26 10:07:07 +0300

    base query updated

--- src/api/baseUrl.ts
+++ src/api/baseUrl.ts
@@ -1,7 +1,7 @@
 import { fetchBaseQuery } from "@reduxjs/toolkit/query";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'https://portfolio-api-b3ur.onrender.com/',
+  baseUrl: 'https://portfolio-api-b3ur.onrender.com',
 });
 
 export default baseQuery
